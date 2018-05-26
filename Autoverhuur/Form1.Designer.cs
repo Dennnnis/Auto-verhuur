dcd6a9d2caf1320a702976c106db32a12ef37a4c
Dennnnis <dennisbeckertim@gmail.com>
2018-05-26 20:22:04 +0200

    Add project files.

---
+++ Autoverhuur/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Autoverhuur
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.CalendarDimensions = new System.Drawing.Size(2, 1);
+            this.monthCalendar1.Location = new System.Drawing.Point(206, 18);
+            this.monthCalendar1.MaxSelectionCount = 900000;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bodoni MT", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(94, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 47);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Autoverhuur Bedrijf";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(16, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(434, 189);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(118, 26);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "01/01/2000";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(434, 221);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "02/01/2000";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(390, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Van";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(396, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tot";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Auto type";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 73);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 189);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(430, 250);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "1 Dag(en)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(434, 296);
+            this.textBox3.MaxLength = 6;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(118, 26);
+            this.textBox3.TabIndex = 14;
+            this.textBox3.Text = "150";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(396, 299);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(32, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Km";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(434, 328);
+            this.textBox4.MaxLength = 20;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(118, 26);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.Text = "00,00";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(305, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Benzine koste €";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(244, 189);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(130, 20);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Ongeldige datum";
+            this.label10.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 425);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "AutoVerhuur";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Autoverhuur/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace Autoverhuur
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        int dagen = 1;
+
+        Dictionary<string, CAuto> autos = new Dictionary<string, CAuto>();
+
+        public decimal Afronding(decimal bedrag) => Math.Round(bedrag, 2);
+     
+        public string EuroTekenEnNull(decimal bedrag)
+        {
+            if (bedrag == 0)
+                return "€ 0,00";
+
+            string stringBedrag = bedrag.ToString();
+
+            while (stringBedrag.Length < Math.Round(bedrag).ToString().Length + 3)
+            {
+                stringBedrag += "0";
+            }
+
+            stringBedrag = stringBedrag.Insert(0, "€ ");
+
+            return stringBedrag;
+        }
+
+        public class CAuto
+        {
+            //Weet niet hoe ik deze 2 moet pakken van Form1 zonder inheritance
+            public decimal Afronding(decimal bedrag) => Math.Round(bedrag, 2);
+            public string EuroTekenEnNull(decimal bedrag)
+            {
+                if (bedrag == 0)
+                    return "€ 0,00";
+
+                string stringBedrag = bedrag.ToString();
+
+
+                while (stringBedrag.Length < Math.Round(bedrag).ToString().Length + 3)
+                {
+                    stringBedrag += "0";
+                }
+
+                stringBedrag = stringBedrag.Insert(0, "€ ");
+
+                return stringBedrag;
+            }
+
+            public decimal kilometerPrijs = 0.0m;
+            public decimal dagtarief = 0.0m;
+            public decimal kilometerVrij = 0.0m;
+
+            public new string ToString()
+            {
+                return "Kilometer prijs: " + (EuroTekenEnNull(Afronding(kilometerPrijs))) + "\n" +
+                       "Dagtarief: " + (EuroTekenEnNull(Afronding(dagtarief))) + "\n" +
+                       "Eerste " + Math.Round(kilometerVrij) + " km vrij";
+                
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            monthCalendar1.MinDate = DateTime.Today;
+            
+            autos.Add("Personenauto", new CAuto() { kilometerPrijs = 0.20m, dagtarief = 50.0m, kilometerVrij = 100.0m });
+            autos.Add("Personenbusje", new CAuto() { kilometerPrijs = 0.30m, dagtarief = 95.0m, kilometerVrij = 100.0m });
+            autos.Add("Vrachtwagen", new CAuto() { kilometerPrijs = 0.50m, dagtarief = 120.0m, kilometerVrij = 200.0m });
+            autos.Add("Limousine", new CAuto() { kilometerPrijs = 1.50m, dagtarief = 500.0m, kilometerVrij = 0.0m });
+            autos.Add("Ferrari", new CAuto() { kilometerPrijs = 2.00m, dagtarief = 1200.0m, kilometerVrij = 0.0m });
+
+            foreach (string name in autos.Keys)
+            {
+                comboBox1.Items.Add(name);
+            }
+
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Bereken()
+        {
+
+            CAuto bAuto = autos[comboBox1.SelectedItem.ToString()];
+
+            decimal geredenKilometers;
+
+            try
+            {
+                geredenKilometers = Convert.ToDecimal(textBox3.Text);
+            }
+            catch(Exception)
+            {
+                geredenKilometers = 0.0m;
+            }
+            
+            
+            decimal geredenKilometerPrijs = Math.Max(0,(geredenKilometers - bAuto.kilometerVrij) * bAuto.kilometerPrijs);
+            decimal dagtariefPrijs = dagen * bAuto.dagtarief;
+            decimal benzinePrijs;
+
+            try
+            {
+                benzinePrijs = Convert.ToDecimal(textBox4.Text);
+            }
+            catch(Exception)
+            {
+                benzinePrijs = 0.0m;
+            }
+
+            label7.Text =
+                "Berekening: \n" +
+                "____________________________\n" +
+                EuroTekenEnNull(Afronding(geredenKilometerPrijs)) + " Voor gereden kilometers\n" +
+                EuroTekenEnNull(Afronding(dagtariefPrijs)) + " Dagtarief\n" +
+                EuroTekenEnNull(Afronding(benzinePrijs)) + " Benzine kosten\n" +
+                "____________________________\n" +
+                "Prijs totaal: " + EuroTekenEnNull(Afronding(dagtariefPrijs + geredenKilometerPrijs + benzinePrijs));
+
+
+
+
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            textBox1.Text = monthCalendar1.SelectionStart.ToShortDateString();
+            textBox2.Text = monthCalendar1.SelectionEnd.ToShortDateString();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (autos.ContainsKey(comboBox1.SelectedItem.ToString()))
+                label6.Text = autos[comboBox1.SelectedItem.ToString()].ToString();
+            Bereken();
+        }
+
+        private void TekstDoosVerandered()
+        {
+            Bereken();
+            try
+            {
+                Convert.ToDateTime(textBox1.Text);
+                Convert.ToDateTime(textBox2.Text);
+            }
+            catch (System.FormatException)
+            {
+                label10.Visible = true;
+                return;
+            }
+            label10.Visible = false;
+            dagen = Math.Max(0,(Convert.ToDateTime(textBox2.Text) - Convert.ToDateTime(textBox1.Text)).Days) + 1;
+                label8.Text = (dagen.ToString() + " Dag(en)");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TekstDoosVerandered();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            TekstDoosVerandered();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (!textBox4.Text.Contains(","))
+            {
+                textBox4.Text += ",00";
+            }
+            Bereken();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bereken();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Bereken();
+        }
+    }
+}
